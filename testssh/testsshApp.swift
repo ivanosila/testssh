24b888261d9e1c0b361aca4fb82e3ddca4d799d4
Ivan Kyser Osila <ivankyser@icloud.com>
2023-07-22 08:28:20 +0800

    Initial Commit

---
+++ testssh/testsshApp.swift
@@ -0,0 +1,17 @@
+//
+//  testsshApp.swift
+//  testssh
+//
+//  Created by Ivan Kyser Osila on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct testsshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
